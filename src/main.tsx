88486069466af19593ccefd531bdc0a14ec08e94
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-04-19 22:24:01 +0200

    Fix images in gh-pages attempt 2

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 
-const basename = process.env.NODE_ENV === "production" ? "/shopping-cart" : "/";
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isProduction ? '/shopping-cart/' : '/',
+  base: '/shopping-cart/',
   server: {
     open: true
   }
