cb0286fecb548ba1f7f6bc5f882cf5b8011a4be3
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-04-19 21:59:39 +0200

    Fix images in gh-pages attempt 1

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
 
+const basename = process.env.NODE_ENV === "production" ? "/shopping-cart" : "/";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
